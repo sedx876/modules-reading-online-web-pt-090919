80b19965cedc7ea0af635e5f0430b8de471f20fa
Sharon Watkins <swatkins876@gmail.com>
2019-10-10 17:21:26 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,10 @@ require_relative './class_methods_module.rb'
 
 class Kid
   
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  extend 
+  FancyDance::ClassMethods
+  include 
+  FancyDance::InstanceMethods
   
   attr_accessor :name 
   
